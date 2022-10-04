1c6bde3e35815a32888edf68362c260806bc5fc5
Amoshe, Richard (k21057746) <richard.amoshe@kcl.ac.uk>
2022-10-04 11:52:00 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError("The number of primes must be greater than 0")
+    list = [2]
+    x = 3
+    while len(list) < number_of_primes:
+        prime = True
+        for i in range(2, x):
+            if x % i == 0:
+                prime = False
+                break
+            i += 1
+        if prime == True:
+            list.append(x)
+        x += 1
     return list
