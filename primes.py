dd630937dcc83c3063a5312e9a2609881546f989
Richard Amoshe <richard.amoshe@kcl.ac.uk>
2023-10-23 15:16:31 +0100

    complete

--- primes.py
+++ primes.py
@@ -7,13 +7,13 @@ def primes(number_of_primes):
     list = [2]
     x = 3
     while len(list) < number_of_primes:
-        prime = True
+        isPrime = True
         for i in range(2, x):
             if x % i == 0:
                 prime = False
                 break
             i += 1
-        if prime == True:
+        if isPrime == True:
             list.append(x)
         x += 1
     return list
